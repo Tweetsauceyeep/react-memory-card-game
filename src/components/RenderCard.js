e4a328204abdc4a200d22d07ac794c911948e330
Tweetsauce <pobusauce12@gmail.com>
2022-02-14 00:33:50 +0400

    array shuffle and score increment done

--- src/components/RenderCard.js
+++ src/components/RenderCard.js
@@ -1,13 +1,17 @@
 import {useState} from 'react';
 
-const RenderCard = ({data}) => {
+const RenderCard = ({data, handleCardClick}) => {
   return (
     <div>
       {data.map(item => {
         return (
-          <div key={item.key}>
+          <div key={item.key} onClick={handleCardClick} style={{display:"flex"}}>
+            <img
+              alt={`${item.name}`}
+              src={require(`../images/${item.image}`)}
+              style={{height:"100px", width:"100px", alignSelf:"center"}}
+            />
             <div>{item.name}</div>
-            <img src={item.image}></img>
           </div>
         );
       })}
@@ -15,4 +19,4 @@ const RenderCard = ({data}) => {
   );
 };
 
-export default RenderCard
+export default RenderCard;
