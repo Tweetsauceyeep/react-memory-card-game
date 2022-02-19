cf465432021e9fa801aeb6b9b6cc0df91a1aa30a
Tweetsauce <pobusauce12@gmail.com>
2022-02-20 01:41:14 +0400

    project finished. add styles and more cards

--- src/App.js
+++ src/App.js
@@ -4,10 +4,36 @@ import Scoreboard from './components/Scoreboard';
 import RenderCard from './components/RenderCard';
 
 let data = require('./cards.json');
+
 function App() {
-  const [cardArray, setCardArray] = useState(data);
+  const [cardArray, setCardArray] = useState([
+    {
+      name: 'Ethan',
+      image: 'christian-buehner-DItYlc26zVI-unsplash.jpg',
+      key: 'Ethan1',
+      clicked: false,
+    },
+    {
+      name: 'Arab man',
+      image: 'irene-strong-v2aKnjMbP_k-unsplash.jpg',
+      key: 'Arabman2',
+      clicked: false,
+    },
+    {
+      name: 'don Qui',
+      image: 'janko-ferlic-G-jo31ESuRE-unsplash.jpg',
+      key: 'Donqui3',
+      clicked: false,
+    },
+    {
+      name: 'shawtee B.',
+      image: 'jonas-kakaroto-KIPqvvTOC1s-unsplash (1).jpg',
+      key: 'shawteeB4',
+      clicked: false,
+    },
+  ]);
   const [score, setScore] = useState(0);
-  const [prevHighScore, setPrevHighScore] = useState(0);
+  const [prevHighScore, setPrevHighScore] = useState([]);
   const [gameLost, setGameLost] = useState(false);
 
   const shuffleArray = oldArray => {
@@ -16,14 +42,15 @@ function App() {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
-
     setCardArray(array);
   };
 
   const handleCardClickedState = e => {
     for (let i = 0; i < cardArray.length; i++) {
       if (cardArray[i].name === e.target.name) {
-        cardArray[i].clicked = true; // idk if mutating state directly is ok but idk try using set state or something
+        // iterate over card array and then do stuff
+        setCardArray([...cardArray, (cardArray[i].clicked = true)]);
+        //cardArray[i].clicked = true; // idk if mutating state directly is ok but idk try using set state or something
         console.log(
           `${e.target.name} has been clicked and clicked value  = ${cardArray[i].clicked}`,
         );
@@ -44,15 +71,68 @@ function App() {
     }
   };
 
+
+  // ran everytime a card is clicked
   const handleFunctionsClicks = e => {
+    console.log(cardArray);
     handleGameLose(e);
     if (gameLost === false) {
       handleCardClickedState(e);
-      shuffleArray([...cardArray]);
       setScore(score + 1);
-    } else if (gameLost === true) {
-      setScore(0);
+      shuffleArray([...cardArray]);
     }
+  };
+
+  const bubbleSort = (array) => {
+    for(let i = 0; i < array.length; i++){
+      for (let j = i; j < array.length; j++){
+        if (array[j] < array[i]){
+          [array[j], array[i]] = [array[i], array[j]]
+        }
+      }
+    }
+    return array[0]
+  }
+
+  const handleHighScore = (prevScore) =>{
+    let highscoreArray = []
+    let newPrevScore = prevScore - 1
+    highscoreArray.push(newPrevScore)
+    setPrevHighScore(bubbleSort(highscoreArray))
+  }
+
+  const handleReset = () => {
+    console.log('game has been reset');
+    console.log(prevHighScore)
+    setCardArray([
+      {
+        name: 'Ethan',
+        image: 'christian-buehner-DItYlc26zVI-unsplash.jpg',
+        key: 'Ethan1',
+        clicked: false,
+      },
+      {
+        name: 'Arab man',
+        image: 'irene-strong-v2aKnjMbP_k-unsplash.jpg',
+        key: 'Arabman2',
+        clicked: false,
+      },
+      {
+        name: 'don Qui',
+        image: 'janko-ferlic-G-jo31ESuRE-unsplash.jpg',
+        key: 'Donqui3',
+        clicked: false,
+      },
+      {
+        name: 'shawtee B.',
+        image: 'jonas-kakaroto-KIPqvvTOC1s-unsplash (1).jpg',
+        key: 'shawteeB4',
+        clicked: false,
+      },
+    ]);
+    setGameLost(!gameLost);
+    handleHighScore(score)
+    setScore(0);
   };
 
   return gameLost === false ? (
@@ -66,6 +146,7 @@ function App() {
   ) : (
     <div>
       <div>YOU LOST BRUH</div>
+      <button onClick={() => handleReset()}>Replay?</button>
     </div>
   );
 }
